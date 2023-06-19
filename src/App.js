a999d3a3462bf3f1bd0a2f5ceaf8bcee0ef3b3e3
asha <ashabadavagol1668@gmail.com>
2023-06-19 19:24:49 +0530

    add file

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import axios from 'axios';
 import './App.css';
 
+
+
 function App() {
+  const [users, setUsers] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  const getUsers = async () => {
+    setLoading(true);
+    try {
+      const response = await axios.get('https://reqres.in/api/users?page=1');
+      setUsers(response.data.data);
+      setLoading(false);
+    } catch (error) {
+      console.log(error);
+      setLoading(false);
+    }
+  };
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <nav className="navbar">
+        <div className="brand">
+          <span>Brand Name</span>
+        </div>
+        <div className="button">
+          <button onClick={getUsers}>Get Users</button>
+        </div>
+      </nav>
+      {loading ? (
+        <div className="loader"></div>
+      ) : (
+        <div className="user-card-grid">
+          {users.map((user) => (
+            <div className="user-card" key={user.id}>
+              <img className="user-avatar" src={user.avatar} alt={user.first_name} />
+              <h3 className="user-name">{`${user.first_name} ${user.last_name}`}</h3>
+              <p className="user-email">{user.email}</p>
+            </div>
+          ))}
+        </div>
+      )}
     </div>
   );
 }
